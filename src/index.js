ccff526c70d76c30446163b19ea0509925d88312
Darien Bernal <darien.bernal@gmail.com>
2025-02-01 21:37:21 -0500

    Create list of colors and randomly pick one from list

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 // Create your game here!
+// Color Guessing Game
+
+// Create list of colors
+const colorList = ["red", "orange", "yellow", "green", "blue", "purple", "pink", "white", "brown", "black"];
+console.log(colorList);
+
+let i = Math.floor(Math.random() * colorList.length);
+let randomColor = colorList[i];
+console.log(randomColor)

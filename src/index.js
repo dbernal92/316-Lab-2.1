d1f728d49eb09d623cafac2edf372a5a5f3be376
Darien Bernal <darien.bernal@gmail.com>
2025-02-02 14:15:58 -0500

    Changing game from colors to superheroes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 // Create your game here!
-// Color Guessing Game
+// Superhero Guessing Game
+
+const title = document.createElement('title');
+title.textContent = "Superhero Guessing Game!";
 
 // Create list of colors
 const colorList = ["red", "orange", "yellow", "green", "blue", "purple", "pink", "white", "brown", "black"];
 console.log(colorList);
 
-let i = Math.floor(Math.random() * colorList.length);
-let randomColor = colorList[i];
+let index = Math.floor(Math.random() * colorList.length);
+let randomColor = colorList[index];
 console.log(randomColor)
+
+// Get User Input
+let userInput = window.prompt("What color dod you think it is?");
+console.log("User entered:", userInput);
